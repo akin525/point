1a799053030811f3d1bd099726df2bf32d58024a
DELL <akinlabisamson15@gmail.com>
2024-05-26 00:07:19 +0100

    FG

--- project/app/Http/Kernel.php
+++ project/app/Http/Kernel.php
@@ -55,7 +55,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithB#5f5af6asicAuth::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
 //        'csmdev' => \App\Http\Middleware\csmdev::class,
